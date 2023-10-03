07e450fd51fcd62392afdafbaebb0da835159aa1
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-10-02 23:14:22 -0700

    Defined a JavaScript function 'getTitle' to display the title in the App component.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@
 
 import React from 'react';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
       <h1>
-        {welcome.greeting} {welcome.title}
+        Hello {getTitle('React')}
       </h1>
 
       <label htmlFor="search">Search: </label>
