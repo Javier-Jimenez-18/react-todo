6721ab10b6be8f9d8ca9ab52ceb04a938b3c9b77
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-10-07 13:10:43 -0700

    Extracted the List component and Search component.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
 import React from 'react';
 
-const todoList = [
+const list = [
   {
-    id: 1,
-    title: 'Read "Hello React", "Requirements", "Setting up a React Component", "Meet the React Component", "React JSX", and "Lists in React"',
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
   }, 
   {
-    id: 2,
-    title: 'Complete Lesson 1.1 exercises',
-  },
-  {
-    id: 3,
-    title: 'Git add, commit, and push code from branch lesson_1_1 to GitHub',
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   }, 
 ];
 
 function App() {
   return (
     <div>
-      <h1>Todo List</h1>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+
       <hr />
-      <ul>
-        {todoList.map(function (item) {
-          return (
-            <li key={item.id}>
-              <span>{item.title}</span>
-            </li>  
-          );
-        })}
-      </ul>  
+
+      <List />  
     </div>
   );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div> 
+  );
+}      
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })} 
+    </ul>
+  ); 
 }
 
 export default App;
