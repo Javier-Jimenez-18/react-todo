1a69ebf6d9944f1ff624e1da61f47da7caf7a445
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-10-07 17:08:15 -0700

    Refactored React component definitions. Changed regular functions to arrow functions. Removed comments.

--- src/App.js
+++ src/App.js
@@ -19,53 +19,38 @@ const list = [
   }, 
 ];
 
-// definition of App component
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      {/* creating an instance of Search component */}
-      <Search />
+    <Search />
 
-      <hr />
+    <hr />
 
-      {/* creating an instance of List component */}
-      <List />  
-      {/* creating another instance of List component */}
-      <List />
-    </div>
-  );
-}
+    <List />
+  </div>
+);
 
-// definition of Search component
-function Search() {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div> 
-  );
-}      
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div> 
+);      
 
-// definition of List component
-function List() {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })} 
-    </ul>
-  ); 
-}
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+); 
 
 export default App;
