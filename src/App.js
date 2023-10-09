0bf930405c82b228e2f3124930e6233ac064bc7d
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-10-08 19:50:30 -0700

    Created "Add Todo Form". Created file AddTodoForm.js and new functional React component AddTodoForm. Added JSX to create text input and submit button elements. Added the AddTodoForm component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 const App = () => (
   <div>
     <h1>Todo List</h1>
+    <AddTodoForm />
     <hr />
     <TodoList />
   </div>
