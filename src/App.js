cad50272a8b17bfdb90beea24ad6835163b2674c
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-10-15 00:17:51 -0700

    Handler Function in JSX. Defined a function for the change event of the Search component input field, called an (event) handler. The handler can be passed to the onChange attribute of the HTML input field for handling user interactions.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }, 
+];
 
 const App = () => (
   <div>
-    <h1>Todo List</h1>
-    <AddTodoForm />
+    <h1>My Hacker Stories</h1>
+
+    <Search />
+
     <hr />
-    <TodoList />
+
+    <List />
   </div>
-);      
+);
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div> 
+  );
+};      
+
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+); 
 
 export default App;
