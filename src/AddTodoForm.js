746e716e202d9010923f2a0caa3dd412421b09e0
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-10-18 11:27:01 -0700

    Handle "Add Todo Form" Submit. Created new function called handleAddTodo that takes event as parameter. Added onSubmit prop to form element and passed the handleAddTodo function by reference.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const AddTodoForm = () => (
-  <form>
-    <label htmlFor="todoTitle">Title </label> 
-    <input id="todoTitle" type="text" />
-    <button type="submit">Add</button>
-  </form>
-);
+const AddTodoForm = (props) => {
+  
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.title.value;
+    console.log(todoTitle);
+    event.target.reset();
+  };
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="todoTitle">Title </label> 
+      <input id="todoTitle" name="title" type="text" />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
 
 export default AddTodoForm;
--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-const App = () => (
-  <div>
-    <h1>Todo List</h1>
-    <AddTodoForm />
-    <hr />
-    <TodoList />
-  </div>
-);      
+const App = () => {
+  
+  const [newTodo, setNewTodo] = React.useState('');
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <AddTodoForm onAddTodo={setNewTodo}/>
+      <p>{newTodo}</p>
+      <hr />
+      <TodoList />
+    </div>
+  );
+};      
 
 export default App;
