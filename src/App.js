9424e15e3d29413e68de984aab18b5624f39eb40
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-11-23 12:54:03 -0700

    Save Todo List in Storage. Updated App.js to save my Todo List in storage so it persists between page loads. Defined a useEffect React hook with todoList as a dependency and savedTodoList key. Updated the default state for todoList to read savedTodoList key item from localStorage.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import AddTodoForm from './AddTodoForm';
 
 const App = () => {
   
-  const [todoList, setTodoList] = React.useState([]);
+  const [todoList, setTodoList] = React.useState(
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
 
   const addTodo = (newTodo) => {   
     setTodoList([...todoList, newTodo]);
