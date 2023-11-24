83533c9498d80e055ef54963d7a43cc56030ef09
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-11-24 15:58:33 -0700

    Fragments. Updated the JSX in App to use React Fragment. It wraps multiple elements into a single top-level element without adding to the rendered output.

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ const App = () => {
   };
 
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo}/>
       <hr />
       <TodoList todoList={todoList}/>
-    </div>
+    </>
   );
 };      
 
