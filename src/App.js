e891f9f292615ce6e34667f0e461230ab44d22ab
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2023-11-25 00:24:01 -0700

    Imperative React. Modified App and InputWithLabel components to execute the focus() method programmatically via the input field element's DOM API once it has been rendered. Applied useRef hook to create ref object with roperty called current that can be changed and assigned it to the element instance. Applied useEffect hook to perform the focus on the input field. Executed its focus programmatically as a side-effect.

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ const App = () => {
       <InputWithLabel
         id="search"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search:</strong>
@@ -61,18 +62,33 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, value, type = 'text', onInputChange, children }) => (
-  <>
-    <label htmlFor={id}> {children} </label>
-    &nbsp;
-    <input 
-      id={id} 
-      type={type}
-      value={value} 
-      onChange={onInputChange} 
-    />
-  </> 
-);      
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
+  // A
+  const inputRef = React.useRef();
+
+  // C
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      // D
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}> {children} </label>
+      &nbsp;
+      {/* B */}
+      <input 
+        ref={inputRef}
+        id={id} 
+        type={type}
+        value={value} 
+        onChange={onInputChange} 
+      />
+    </> 
+  );
+};        
 
 const List = ({ list }) => (
   <ul>
